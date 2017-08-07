ade5268bb3291b5c1fb6b34ff6e68795fbffce7b
Administrator <abhi_sri_amity6@yahoo.com>
2017-08-08 01:46:34 +0530

    completed

--- FileStream-Demo/Program.cs
+++ FileStream-Demo/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Security.AccessControl;
 using System.Threading.Tasks;
 
 namespace FileStream_Demo
@@ -10,6 +12,38 @@ namespace FileStream_Demo
     {
         static void Main(string[] args)
         {
+
+            FileStream fin = null;
+            try
+            {
+                fin = new FileStream("c:\\temp\\test.txt", FileMode.Open);
+
+              FileSecurity AC =   fin.GetAccessControl();
+              AuthorizationRuleCollection ACLs =   AC.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+                foreach (FileSystemAccessRule ACL in ACLs)
+                {
+                    Console.WriteLine("Account : " + ACL.IdentityReference.Value);
+                    Console.WriteLine("Type : " + ACL.AccessControlType);
+                    Console.WriteLine("Rights : " + ACL.FileSystemRights);
+                    Console.WriteLine("Inherited ACE : " + ACL.IsInherited);
+                    Console.WriteLine("========================================================\n");
+
+
+                }
+
+            }
+            catch (IOException exc)
+            { // catch all I/O exceptions
+                Console.WriteLine(exc.Message);
+                // Handle the error.
+            }
+            catch (Exception exc)
+            { // catch any other exception
+                Console.WriteLine(exc.Message);
+                // Handle the error, if possible.
+                // Rethrow those exceptions that you don't handle.
+            }
+            Console.Read();
         }
     }
 }
